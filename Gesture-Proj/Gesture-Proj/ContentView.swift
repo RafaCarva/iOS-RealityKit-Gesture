169e704e353a3c5262fa8301e2f353f355933eb5
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2022-08-29 11:13:59 -0300

    Insere múltiplos objetos na tela usando como referência de location o raycast.

--- Gesture-Proj/Gesture-Proj/ContentView.swift
+++ Gesture-Proj/Gesture-Proj/ContentView.swift
@@ -19,38 +19,24 @@ struct ARViewContainer: UIViewRepresentable {
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
-                arView.addGestureRecognizer(UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleTap)))
-                
-                //Fazer o coordenator saber sobre a view:
-                //Aqui você está passando esse arView local para a var do tipo view de Coordinator.
-                context.coordinator.view = arView
-                
-                // Todos os eventos dessa ARView session vai ser delegada para coordinator, que por sua vez, tratará esses eventos.
-                arView.session.delegate = context.coordinator
-                
-                
-                //criar uma ancora horizontal
-                let anchor = AnchorEntity(plane: .horizontal)
-                
-                // o box com material
-                let material = SimpleMaterial(color: .blue, isMetallic: true)
-                let box = ModelEntity(mesh: MeshResource.generateBox(size: 0.3), materials: [material])
-                box.generateCollisionShapes(recursive: true)
-                
-                anchor.addChild(box)
-                arView.scene.anchors.append(anchor)
-
-                // Esse arView possúi um gestureRecognizer associado ao "Coordinator".
-                return arView
-
+        arView.addGestureRecognizer(UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleTap)))
+        
+        //Fazer o coordenator saber sobre a view:
+        //Aqui você está passando esse arView local para a var do tipo view de Coordinator.
+        context.coordinator.view = arView
+        
+        // Todos os eventos dessa ARView session vai ser delegada para coordinator, que por sua vez, tratará esses eventos.
+        arView.session.delegate = context.coordinator
+        
+        // Esse arView possúi um gestureRecognizer associado ao "Coordinator".
+        return arView
         
     }
     
     // Essa func vem de UIViewReprsentable
-        func makeCoordinator() -> Coordinator {
-            Coordinator()
-        }
-
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
--- Gesture-Proj/Gesture-Proj/Coordinator.swift
+++ Gesture-Proj/Gesture-Proj/Coordinator.swift
@@ -24,19 +24,29 @@ class Coordinator: NSObject, ARSessionDelegate{
         // Quando dá o tap, pega a location
         let tapLocation = recognizer.location(in: view)
         
-        //Aqui ele faz um caast do valor para ModelEntity.
-        if let entity = view.entity(at: tapLocation) as? ModelEntity {
-            
-            // Esse UIColor.random() vem do UIColor+Extensions.swift
-            let material = SimpleMaterial(
-                color: UIColor.randomColor(),
-                isMetallic: true
-            )
-            entity.model?.materials = [material]
-        }
+        // O raycast é como um "raio" que sai da tela do celular.
+        let results = view.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal)
         
+        // Se results tiver detectado algum plano:
+        if let result = results.first {
+            //Anchor
+            let anchor = ARAnchor(name: "Plane Anchor", transform: result.worldTransform)
+            view.session.add(anchor: anchor)
+            
+            //Objeto 3D
+            let modelEntity = ModelEntity(mesh: MeshResource.generateBox(size: 0.15))
+            modelEntity.model?.materials = [SimpleMaterial(color: UIColor.randomColor(), isMetallic: true)]
+            
+            //Anchor Entity
+            //Aqui você linka o "Anchor" no "Anchor Entity", e depois o "modelEntity" no "AnchorEntity".
+            let anchorEntity = AnchorEntity(anchor: anchor)
+            anchorEntity.addChild(modelEntity)
+            
+            //Por fim, vc add seu "anchorEntity"(que já possúi um nó com o objeto 3D) na cena.
+            view.scene.addAnchor(anchorEntity)
+            
+        }
+  
     }
 
-    
-    
 }
